d88592118aa961d56e4f8f6b89506980795a2fe8
xbgbtx <xbgbtx@xbgbtx.com>
2024-09-29 15:49:37 +0100

    Add simple drawing sketch

---
+++ sketches/simple-drawing.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  circle(200, 100, 100);
+  
+  rect (150, 150, 100, 150);
+  
+  line(150, 150, 80, 190);
+
+  line(250, 150, 330, 190);
+  
+  line(150, 300, 150, 375);
+
+  line(250, 300, 250, 375);
+}
