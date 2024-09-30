3dc90409d9f7787c0526f292a9c6acf4a069a0f9
JoyfulDecay <joyful.decay@gmail.com>
2024-09-30 19:26:28 +0100

    Add path

--- sketches/color.js
+++ sketches/color.js
@@ -5,10 +5,6 @@ function setup() {
 function draw() {
   background(135, 206, 235);
   
-  //ground
-  fill(77, 235, 55);
-  rect(0,250, 400,400);
-  
   //clouds
   fill(255,255,255);
   strokeWeight(0);
@@ -16,12 +12,21 @@ function draw() {
   ellipse(70, 30, 180, 30);
   ellipse(230, 20, 100, 30);
   
+    
+  //ground
+  strokeWeight(2);
+  fill(77, 235, 55);
+  rect(0,250, 400,400);
+  
   //house
   fill(200, 180, 77);
-  strokeWeight(1);
   rect(160,200, 80);
   
   //roof
   fill(255, 100, 90);
   triangle(160,200, 200, 150, 240, 200)
+  
+  //path
+  fill(205, 220, 215);
+  quad(195, 280, 205, 280, 225, 400, 175, 400);
 }
