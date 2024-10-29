869897c07114315076dff9609af82be088d41cea
xbgbtx <xbgbtx@xbgbtx.com>
2024-10-29 17:09:23 +0000

    Move bar_chart draw to class

--- chapters/002-randomness/random-distribution-barchart.js
+++ chapters/002-randomness/random-distribution-barchart.js
@@ -1,7 +1,7 @@
 let w = 400;
 let h = 400;
 
-let bar_chart;
+let random_bar_chart;
 
 class BarChart {
   constructor(num_bars) {
@@ -23,26 +23,30 @@ class BarChart {
   get_value(idx) {
     return this.bars[idx];
   }
+
+  draw(fill_color) {
+    for(let i=0; i<this.num_bars; i++) {
+      let x0 = (w/this.num_bars) * i;
+      let y0 = h;
+
+      let x1 = x0 + w/this.num_bars;
+      let y1 = h - this.get_value(i);
+
+      fill(fill_color);
+      rect(x0, y0, x1, y1);
+    }
+  }
 }
 
 function setup() {
   rectMode(CORNERS);
   createCanvas(w, h);
-  bar_chart = new BarChart(32);
+  random_bar_chart = new BarChart(32);
 }
 
 function draw() {
   background(220);
 
-  for(let i=0; i<bar_chart.num_bars; i++) {
-    let x0 = (w/bar_chart.num_bars) * i;
-    let y0 = h;
-
-    let x1 = x0 + w/bar_chart.num_bars;
-    let y1 = h - bar_chart.get_value(i);
-
-    rect(x0, y0, x1, y1);
-  }
-
-  bar_chart.add_value(random());
+  random_bar_chart.add_value(random());
+  random_bar_chart.draw('rgb(52,140,174)');
 }
