7e096f86cf6b46dfae37f878795dc1c74f8fff5a
xbgbtx <xbgbtx@xbgbtx.com>
2024-10-30 17:29:04 +0000

    demo_random_gaussian.js

---
+++ chapters/002-randomness/demo_random_gaussian.js
@@ -0,0 +1,30 @@
+let w = 800;
+let h = 800;
+
+let mean = 10;
+let dev = 100;
+
+function setup() {
+  createCanvas(w, h);
+  noLoop();
+}
+
+function random_pixel_color() {
+  let r = randomGaussian(mean, dev);
+  return [r, r, r, 255];
+}
+
+function draw() {
+  loadPixels();
+  let num_pixels = pixels.length;
+
+  for (let i = 0; i < num_pixels; i += 4) {
+    let r = random_pixel_color();
+    pixels[i] = r[0];
+    pixels[i + 1] = r[1];
+    pixels[i + 2] = r[2];
+    pixels[i + 3] = r[3];
+  }
+
+  updatePixels();
+}
